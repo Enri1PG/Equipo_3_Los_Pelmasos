39b67f1757c04502b60182c6a01248abb05ceac8
julian <jjvasquezr24@gmail.com>
2025-08-12 09:10:33 -0600

    Update main.dart
    se iso el main

--- main.dart
+++ main.dart
@@ -1,1 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:proyecto_videoteca/LogIn.dart';
+
+
+
+void main() => runApp(farmacia_App());
+
+class farmacia_App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: LoginPage.id,
+      routes: {LoginPage.id: (context) => LoginPage()},
+    );
+  }
+}
 
